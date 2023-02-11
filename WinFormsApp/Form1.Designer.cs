2080e696d1978da88f33680a2737d4e200863342
Miljan <miljanpjanovic@hotmail.com>
2023-02-11 18:07:04 +0100

    Added WinForms application

---
+++ WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listaImena = new System.Windows.Forms.ListView();
+            this.username = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.firstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnMouseClick);
+            // 
+            // listaImena
+            // 
+            this.listaImena.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.username,
+            this.firstName,
+            this.lastName,
+            this.email});
+            this.listaImena.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaImena.HideSelection = false;
+            this.listaImena.Location = new System.Drawing.Point(145, 54);
+            this.listaImena.Name = "listaImena";
+            this.listaImena.Size = new System.Drawing.Size(606, 384);
+            this.listaImena.TabIndex = 1;
+            this.listaImena.UseCompatibleStateImageBehavior = false;
+            this.listaImena.View = System.Windows.Forms.View.Details;
+            // 
+            // username
+            // 
+            this.username.Text = "Username";
+            this.username.Width = 150;
+            // 
+            // firstName
+            // 
+            this.firstName.Text = "First Name";
+            this.firstName.Width = 150;
+            // 
+            // lastName
+            // 
+            this.lastName.Text = "Last Name";
+            this.lastName.Width = 150;
+            // 
+            // email
+            // 
+            this.email.Text = "Email";
+            this.email.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 519);
+            this.Controls.Add(this.listaImena);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listaImena;
+        private System.Windows.Forms.ColumnHeader username;
+        private System.Windows.Forms.ColumnHeader firstName;
+        private System.Windows.Forms.ColumnHeader lastName;
+        private System.Windows.Forms.ColumnHeader email;
+    }
+}
+
---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:7209/api/user/");
+            try
+            {
+                var response = await httpClient.GetAsync("getusers");
+                if (response != null)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    var users = JsonConvert.DeserializeObject<IEnumerable<User>>(result);
+
+                    if (users != null)
+                    {
+                        foreach (var user in users)
+                        {
+                            string[] returnedUsers = { user.UserName, user.FirstName, user.LastName, user.Email };
+                            var listViewItems = new ListViewItem(returnedUsers);
+                            listaImena.Items.Add(listViewItems);
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
